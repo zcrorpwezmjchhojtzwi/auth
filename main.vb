71283098335d8c90ba480e1883ad607cee1e984a
scriptfree <104465644+scriptfree@users.noreply.github.com>
2024-07-12 00:19:43 -0700

    Update main.vb

--- main.vb
+++ main.vb
@@ -2,4 +2,5 @@ return {
     ["861919BC-DFB3-4EA0-ABE0-EC37DE269814"] = true, -- 986852436587196416 or mommy
     ["423f1139-1d8f-4f24-ad23-20f39f49ff88"] = true, -- Lyrical
     ["DDD64905-8CD7-4ED1-AB76-B44FBC1072B8"] = true, -- xubs
+    ["AB2701F0-E023-436E-BC95-7A49791DEBEA"] = true, -- shite (My brother)
 }
