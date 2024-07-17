f4ad884aad09cb90ceaf1cafbfac8d93b8145005
zcrorpwezmjchhojtzwi <jeffreydecompiler@gmail.com>
2024-07-16 17:09:17 -0700

    Update main.vb

--- main.vb
+++ main.vb
@@ -2,12 +2,7 @@ return {
     ["861919BC-DFB3-4EA0-ABE0-EC37DE269814"] = true, -- 986852436587196416 or mommy
     ["CC091CA6-4BB0-4E68-9718-E0EE376D8B0B"] = true, -- 1041043066749976616 or eugene
     ["dfde0452-77b7-44cc-b2af-f2103320f5c2"] = true, -- syns
-    ["3d08910f-2db8-4e59-94fb-d39838310359"] = true, -- lyrical
+    ["d0078bde-3c08-45a5-a48e-a9f9b500e332"] = true, -- lyrical
     ["DDD64905-8CD7-4ED1-AB76-B44FBC1072B8"] = true, -- xubs
     ["AB2701F0-E023-436E-BC95-7A49791DEBEA"] = true, -- xubs brother
-
-
-
-
-    --Xub's android emulator accounts
 }
